4ec81a8e55d13532f7b65cbaaf8813ac3051c343
Sunny Kumar <sunny@techformation.co.in>
2024-09-13 18:17:43 +0530

    google authentication

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export default NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          redirect_uri: 'http://localhost:3000/login'
+        }
+      }
+    }),
+    // Add other providers here
+  ],
+  // Optional: Customize pages, callbacks, etc.
+});
---
+++ src/pages/protected.js
@@ -0,0 +1,21 @@
+// pages/protected.js
+import { getSession } from 'next-auth/react';
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { session },
+  };
+}
+
+export default function ProtectedPage({ session }) {
+  return <div>Welcome, {session.user.name}</div>;
+}
