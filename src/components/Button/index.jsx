2976d0abc3a89aa96edf7e064d40f7e0cb615a1b
Sunny Kumar <sunny@techformation.co.in>
2024-09-19 12:22:38 +0530

    template selection

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./style.css";
-const CustomButton = ({ children, type = "white", buttonStyle, onClick }) => {
+const CustomButton = ({ children, type = "white", buttonStyle, onClick, disabled }) => {
   return (
     <button
       style={buttonStyle}
       onClick={onClick}
+      disabled={disabled}
       className={
         type === "white"
           ? "white-button"
