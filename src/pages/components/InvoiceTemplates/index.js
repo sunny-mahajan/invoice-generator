eb666612a5aa62355e6d05117c83747df9d631c9
Sunny Kumar <sunny@techformation.co.in>
2024-10-08 12:18:51 +0530

    Minor changes

--- src/pages/components/InvoiceTemplates/index.js
+++ src/pages/components/InvoiceTemplates/index.js
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const InvoiceTemplates = ({ handleSelectTemplates = () => {}, selectable = false }) => {
+const InvoiceTemplates = ({
+  handleSelectTemplates = () => {},
+  selectable = false,
+}) => {
   const [selectedTemplateId, setSelectedTemplateId] = useState(null);
   const [invoiceTemplates, setTemplates] = useState([]);
   const [visibleIndex, setVisibleIndex] = useState(0);
@@ -11,11 +14,11 @@ const InvoiceTemplates = ({ handleSelectTemplates = () => {}, selectable = false
   }, []);
 
   const fetchTemplates = async () => {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_PORT}/api/templates`
-    );
+    const res = await fetch(`/api/templates`);
     const templates = await res.json();
+
     setTemplates(templates);
+
     if (templates.length > 0) {
       setSelectedTemplateId(templates[0].id);
       handleSelectTemplates(templates[0].id);
@@ -43,17 +46,19 @@ const InvoiceTemplates = ({ handleSelectTemplates = () => {}, selectable = false
 
   const styles = {
     title: {
-      padding: '35px 0px',
+      padding: "35px 0px",
     },
-    'template-preview-image': {
-      height: '200px', // Set fixed height
-      width: '100%',    // Set width to fill the container
+    "template-preview-image": {
+      height: "200px", // Set fixed height
+      width: "100%", // Set width to fill the container
     },
   };
 
   return (
     <div>
-      <h2 style={styles.title}>{selectable ? 'Select Template' : 'Templates'}</h2>
+      <h2 style={styles.title}>
+        {selectable ? "Select Template" : "Templates"}
+      </h2>
 
       <div className="flex items-center">
         <button
@@ -65,36 +70,40 @@ const InvoiceTemplates = ({ handleSelectTemplates = () => {}, selectable = false
         </button>
 
         <div className="overflow-x-auto flex space-x-4 pb-4">
-          {invoiceTemplates.slice(visibleIndex, visibleIndex + templatesToShow).map((invoiceTemplate) => (
-            <div
-              key={invoiceTemplate.id}
-              className="template-tile w-1/4 p-2 flex items-center flex-col cursor-pointer"
-              onClick={() => selectTemplate(invoiceTemplate.id)}
-            >
+          {invoiceTemplates
+            .slice(visibleIndex, visibleIndex + templatesToShow)
+            .map((invoiceTemplate) => (
               <div
-                className={`template-content flex items-center flex-col ${
-                  selectedTemplateId === invoiceTemplate.id && selectable
-                    ? 'selected-invoice-template'
-                    : ''
-                }`}
+                key={invoiceTemplate.id}
+                className="template-tile w-1/4 p-2 flex items-center flex-col cursor-pointer"
+                onClick={() => selectTemplate(invoiceTemplate.id)}
               >
-                <div className="template-preview-image-container w-full h-48 overflow-hidden">
-                  <img
-                    className="template-preview-image object-cover h-full w-full"
-                    src={invoiceTemplate.previewUrl}
-                    alt={invoiceTemplate.name}
-                  />
-                </div>
-                <div className="template-name">
-                  {selectable ? (
-                    <span>{invoiceTemplate.name}</span>
-                  ) : (
-                    <span style={{ fontSize: '12px' }}>Template Id: {invoiceTemplate.id}</span>
-                  )}
+                <div
+                  className={`template-content flex items-center flex-col ${
+                    selectedTemplateId === invoiceTemplate.id && selectable
+                      ? "selected-invoice-template"
+                      : ""
+                  }`}
+                >
+                  <div className="template-preview-image-container w-full h-48 overflow-hidden">
+                    <img
+                      className="template-preview-image object-cover h-full w-full"
+                      src={invoiceTemplate.previewUrl}
+                      alt={invoiceTemplate.name}
+                    />
+                  </div>
+                  <div className="template-name">
+                    {selectable ? (
+                      <span>{invoiceTemplate.name}</span>
+                    ) : (
+                      <span style={{ fontSize: "12px" }}>
+                        Template Id: {invoiceTemplate.id}
+                      </span>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
 
         <button
@@ -110,6 +119,3 @@ const InvoiceTemplates = ({ handleSelectTemplates = () => {}, selectable = false
 };
 
 export default InvoiceTemplates;
-
-
-
