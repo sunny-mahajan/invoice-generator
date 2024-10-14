fe0857a7a5335e78ff1b5b8e8f176bb3ad6685e0
Sunny Kumar <sunny@techformation.co.in>
2024-10-14 14:50:56 +0530

    check generate-pdf API

--- src/pages/api/generate-pdf/index.js
+++ src/pages/api/generate-pdf/index.js
@@ -1,19 +1,27 @@
 const convertHTMLToPDF = require("pdf-puppeteer");
 
 export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
   try {
     const { HTMLTemplate } = req.body;
+    console.log(req.method, "method------------------");
+    // convertHTMLToPDF(
+    //   HTMLTemplate,
+    //   (pdf) => {
+    //     res.setHeader("Content-Type", "application/pdf");
+    //     res.send(pdf);
+    //   },
+    //   { printBackground: true },
+    //   null,
+    //   true
+    // );
 
-    convertHTMLToPDF(
-      HTMLTemplate,
-      (pdf) => {
-        res.setHeader("Content-Type", "application/pdf");
-        res.send(pdf);
-      },
-      { printBackground: true },
-      null,
-      true
-    );
+    // res.setHeader("Content-Type", "application/pdf");
+
+    // res.send(HTMLTemplate);
+    res.send(`Hello, World! ${req.method}`);
   } catch (error) {
     console.error(error);
     res
