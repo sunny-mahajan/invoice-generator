340da25672d423c1a0554965bd0b7619e69eadc4
Sunny Kumar <sunny@techformation.co.in>
2024-10-18 15:41:51 +0530

    verify post method issue

--- src/pages/api/generate-pdf/index.js
+++ src/pages/api/generate-pdf/index.js
@@ -1,21 +1,32 @@
 const convertHTMLToPDF = require("pdf-puppeteer");
+
 export default async function handler(req, res) {
+  // Set CORS headers to allow cross-origin requests
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS"); // Allow only POST and OPTIONS methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow Content-Type headers
+
+  // Handle OPTIONS preflight request
+  if (req.method === "OPTIONS") {
+    // End the preflight request here (No further action needed for OPTIONS request)
+    return res.status(200).end();
+  }
+
   if (req.method === "POST") {
-    // Check if the method is POST
     try {
       const { HTMLTemplate } = req.body;
 
-      // convertHTMLToPDF(
-      //   HTMLTemplate,
-      //   (pdf) => {
-      //     res.setHeader("Content-Type", "application/pdf");
-      //     res.send(pdf);
-      //   },
-      //   { printBackground: true },
-      //   null,
-      //   true
-      // );
-      res.send(HTMLTemplate);
+      convertHTMLToPDF(
+        HTMLTemplate,
+        (pdf) => {
+          console.log("PDF generated successfully");
+          res.setHeader("Content-Type", "application/pdf");
+          res.send(pdf);
+        },
+        { printBackground: true },
+        null,
+        true
+      );
     } catch (error) {
       console.error(error);
       res
